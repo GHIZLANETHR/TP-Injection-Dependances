a0634c5f328c0d8ed75a54f1d11c08d17d2066ae
Ghizlanethr <touahrighizlane98@gmail.com>
2026-02-22 15:41:39 +0000

    Création interface IMetier et son implémentation

--- TP1/src/main/java/net/ghizlane/metier/MetierImp.java
+++ TP1/src/main/java/net/ghizlane/metier/MetierImp.java
@@ -4,10 +4,21 @@ import net.ghizlane.dao.IDao;
 
 public class MetierImp implements  IMetier{
     private IDao dao;
+
+    public MetierImp(IDao dao) {
+        this.dao = dao;
+    }
+
+    public MetierImp() {
+    }
+
     @Override
     public double calcul() {
         double t = dao.getData();
         double res = t * 12 * Math.PI * Math.cos(t);
         return res;
+    }
+    public void setDao(IDao dao) {
+        this.dao = dao;
     }
 }
