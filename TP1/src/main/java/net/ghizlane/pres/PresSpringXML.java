a988ba09406db14252f024622d68295742c429b9
Ghizlanethr <touahrighizlane98@gmail.com>
2026-02-28 12:11:26 +0000

    Création classe PresSpringXML pour injection Spring via XML

---
+++ TP1/src/main/java/net/ghizlane/pres/PresSpringXML.java
@@ -0,0 +1,13 @@
+package net.ghizlane.pres;
+
+import net.ghizlane.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PresSpringXML {
+    public static void main(String[] args) {
+        ApplicationContext springContext = new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier = springContext.getBean(IMetier.class);
+        System.out.println("Resultat = " + metier.calcul());
+    }
+}
--- TP1/src/main/java/net/ghizlane/pres/pres2.java
+++ TP1/src/main/java/net/ghizlane/pres/pres2.java
@@ -1,19 +1,24 @@
 package net.ghizlane.pres;
 
 import net.ghizlane.dao.IDao;
+import net.ghizlane.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class pres2 {
-    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Scanner scanner = new Scanner(new File("config.txt"));
         String daoClassName = scanner.nextLine();
         Class cDao = Class.forName(daoClassName);
-        IDao dao= (IDao) cDao.newInstance();
-        System.out.println(dao.getData());
+        IDao d= (IDao) cDao.newInstance();
 
+        String metierClassName = scanner.nextLine();
+        Class cmetier = Class.forName(metierClassName);
+        IMetier metier= (IMetier)cmetier.getConstructor(IDao.class).newInstance(d);
+        System.out.println("Resultat = " + metier.calcul());
     }
 }
