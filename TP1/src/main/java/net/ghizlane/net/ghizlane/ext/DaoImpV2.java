30d98ff41eb744120111bcc5ac9474163c7a2f8d
Ghizlanethr <touahrighizlane98@gmail.com>
2026-02-28 10:56:28 +0000

    Création de 2 ème implémentation version capteurs

---
+++ TP1/src/main/java/net/ghizlane/net/ghizlane/ext/DaoImpV2.java
@@ -0,0 +1,12 @@
+package net.ghizlane.net.ghizlane.ext;
+
+import net.ghizlane.dao.IDao;
+
+public class DaoImpV2 implements IDao {
+    @Override
+    public double getData() {
+        System.out.println("Version capteurs .....");
+        double t = 12;
+        return t;
+    }
+}
